f1ba2c93829928e9118385a59a7fb9e4421b4212
suupreme <daniel.cheng@ufl.edu>
2025-11-18 16:32:02 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/suupreme/lab11-DC-SK
+# Partner 1: Daniel Cheng
+# Partner 2: Sean Kwon
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +9,35 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+
+import math
+
+
 def add(a, b): 
-    pass
+    return a + b
 
 
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError
+    if b <= 0:
+        raise ValueError
+    return math.log(b) / math.log(a)
+
+
+def exponent(a, b):
+    return a ** b
